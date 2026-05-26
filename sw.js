d3dd398153999cddc281b4434dbb5310607365ea
gstutorial <gobind.bngn@gmail.com>
2026-05-27 01:05:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,30 @@
-self.addEventListener(
-'install',
-event=>{
+const CACHE_NAME = 'union-checker-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json',
+  'https://cdn.sheetjs.com/xlsx-0.20.2/package/dist/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.31/jspdf.plugin.autotable.min.js'
+];
 
-    console.log(
-    'Service Worker Installed'
-    );
+// Install the Service Worker and Cache Files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
+// Serve Cached Files when Offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
